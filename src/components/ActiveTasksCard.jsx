c2ab229e7afe00946657f6a8972a24c23d38735c
Mahery Zo RSLF <andyjhonna1@gmail.com>
2025-10-17 16:06:06 +0300

    ActiveTasksCard.jsx

--- src/components/ActiveTasksCard.jsx
+++ src/components/ActiveTasksCard.jsx
@@ -3,7 +3,7 @@ import TaskItem from './TaskItem';
 
 const ActiveTasksCard = ({ tasks, toggleTask, deleteTask }) => {
   return (
-    <div className="rounded-2xl shadow-lg p-6 bg-gradient-to-br from-indigo-400/30 to-purple-400/20 dark:from-gray-900/40 dark:to-gray-800/20 border border-white/10 backdrop-blur-lg transition-all hover:scale-[1.01]">
+    <div className="rounded-2xl shadow-lg p-6 bg-gradient-to-br from-indigo-400/30 to-purple-400/20  border border-white/10 backdrop-blur-lg transition-all hover:scale-[1.01]">
       <h2 className="text-2xl font-semibold mb-4 text-white drop-shadow-sm">Tâches Actives</h2>
       <ul className="space-y-3">
         {tasks.map(task => (
